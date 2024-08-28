d7c8442b0050124de32657551219d57be22d03c9
0hzL <arin980921@naver.com>
2024-08-29 06:58:31 +0900

    docs : Email 메소드 주석 추가

--- src/main/java/com/sparta/ecommerce/global/email/controller/EmailController.java
+++ src/main/java/com/sparta/ecommerce/global/email/controller/EmailController.java
@@ -3,15 +3,18 @@ package com.sparta.ecommerce.global.email.controller;
 import com.sparta.ecommerce.global.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/email")
 public class EmailController {
 
     private final EmailService emailService;
 
+    /* 인증번호 발송 */
     @GetMapping("/sendverificationcode")
     public String sendVerificationCode(@RequestParam String email) {
         String verificationCode = emailService.sendVerificationCode(email);
